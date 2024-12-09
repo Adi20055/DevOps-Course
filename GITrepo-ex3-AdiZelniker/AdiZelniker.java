250dd71b665324d5e842ad58985c3e062f1e3fe6
Adi20055 <adi.zelniker2@gmail.com>
2024-12-09 19:46:26 +0200

    exercise 3 after adi change

--- GITrepo-ex3-AdiZelniker/AdiZelniker.java
+++ GITrepo-ex3-AdiZelniker/AdiZelniker.java
@@ -1,7 +1,7 @@
 public class AdiZelniker{
 	public static void main(String[] args) {
 		for (int i=0;i<5; i++) {
-			System.out.println("Adi Zelniker");
+			System.out.println("https://tinyurl.com/y7za8y48");
 		}
 	}
 }
